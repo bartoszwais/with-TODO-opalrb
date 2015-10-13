ddc6f5a580f6e55522e661d43e51fdc413a82557
BW <bartoszwais@googlemail.com>
2015-10-13 17:24:57 +0200

    -todo.rb
    application-todo.rb created, ‘main’ element changed to ‘div’.

--- app/application.rb
+++ app/application.rb
@@ -1,21 +1,7 @@
 require 'opal'
 require 'opal-jquery'
+require_relative 'application-todo.rb'
 
 Document.ready? do
-  list = Document.find('#list')
-  form = Document.find('#form')
-  text_box = form.find('input')
-
-  form.on(:submit) do |event|
-    event.prevent
-
-    item = Element.new('div.row')
-    item.html = "<div class='card'><p>#{text_box.value}</p></div>"
-    item.on(:click) { item.remove }
-
-    list.append(item)
-
-    text_box.value = ''
-
-  end
+    TODO.new
 end
